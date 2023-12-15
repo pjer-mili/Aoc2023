c7d72bd455b9b9a9fe1a23fee35c15da02db7abf
PetarMilisavljevic <milisavljevicpetar6@gmail.com>
2023-12-15 20:36:46 +0100

    day 2 part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,67 +1,74 @@
-use std::{fs::File, io::{BufReader, BufRead}, usize};
+use std::{collections::HashMap, fs::File, io::{BufRead, BufReader}};
 
+#[derive(PartialEq,Eq, Hash, Debug)]
+struct Point(usize, usize);
 fn main() {
     let file = File::open("input.txt").expect("Cannot open file");
     let reader = BufReader::new(file);
 
-    let mut sum = 0;
+    let mut gear_map: HashMap<Point, Vec<String>> = HashMap::new();
 
-    let symbols = vec!['*', '=', '+', '/', '#', '-', '&', '$', '@', '%'];
+    let gear_symbols = vec!['*'];
     let lines: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
 
     for (i, _) in lines.iter().enumerate() {
-        sum = sum + get_line_sum(&i, &lines, &symbols);
+        handle_line(i, &lines, &gear_symbols, &mut gear_map);
     }
 
-    println!("The sum of numbers adjecent to symbols is: {}", sum);
+    let sum = gear_map.values()
+    .filter(|array| array.len() == 2)
+    .map(|array| array.iter().fold(1, |acc, g|{
+        acc * g.parse::<u32>().unwrap()
+    }))
+    .fold(0, |acc, v| {
+        acc + v
+    });
+
+    println!("The sum of gears is: {}", sum);
 }
 
-fn get_line_sum(i: &usize, lines: &Vec<String>, symbols: &Vec<char>) -> u32 {
-    let line = &lines[*i];
+fn handle_line(
+    i: usize,
+    lines: &Vec<String>,
+    gear_symbols: &Vec<char>,
+    gear_map: &mut HashMap<Point, Vec<String>>,
+) {
+    let line = &lines[i];
 
-    let mut partial_sum = 0;
     let mut current_number = String::new();
     let mut start_index: Option<usize> = None;
     let mut end_index: Option<usize> = None;
 
     for (j, c) in line.char_indices() {
-        if c.is_digit(10){
+        if c.is_digit(10) {
             update_indices(&mut start_index, &mut end_index, j);
             current_number.push(c);
 
             if j == line.len() - 1 {
-                    match (start_index, end_index) {
-                        (Some(start), Some(end)) => {
-                            let has_adjecent = check_adjacent(*i, start, end, lines, symbols);
-                            if has_adjecent {
-                                partial_sum = partial_sum + current_number.parse::<u32>().unwrap();
-                            }
-                            clear_params(&mut start_index, &mut end_index, &mut current_number);
-                        },
-                        _ => ()
+                match (start_index, end_index) {
+                    (Some(start), Some(end)) => {
+                        fill_adjacent_gears(i,start,end,lines,gear_symbols,&current_number, gear_map);
+                        clear_params(&mut start_index, &mut end_index, &mut current_number);
                     }
+                    _ => (),
+                }
             }
         } else {
-                if current_number.len() > 0 {
-                    match (start_index, end_index) {
-                        (Some(start), Some(end)) => {
-                            let has_adjecent = check_adjacent(*i, start, end, lines, symbols);
-                            if has_adjecent {
-                                partial_sum = partial_sum + current_number.parse::<u32>().unwrap();
-                            }
-                            clear_params(&mut start_index, &mut end_index, &mut current_number);
-                        },
-                        _ => ()
+            if !current_number.is_empty() {
+                match (start_index, end_index) {
+                    (Some(start), Some(end)) => {
+                        fill_adjacent_gears(i ,start,end,lines, gear_symbols, &current_number, gear_map);
+                        clear_params(&mut start_index, &mut end_index, &mut current_number);
                     }
-                }    
+                    _ => (),
+                }
+            }
         }
     }
-    partial_sum
 }
 
-
-fn update_indices(start: &mut Option<usize>, end: &mut Option<usize>, index: usize){
-    if let None = start{
+fn update_indices(start: &mut Option<usize>, end: &mut Option<usize>, index: usize) {
+    if start.is_none() {
         *start = Some(index);
     }
     *end = Some(index);
@@ -73,48 +80,45 @@ fn clear_params(start: &mut Option<usize>, end: &mut Option<usize>, current_numb
     current_number.clear();
 }
 
-fn check_adjacent(i: usize, start_index: usize, end_index: usize, lines: &Vec<String>, symbols: &Vec<char>) -> bool {
-
+fn fill_adjacent_gears(i: usize, start_index: usize, end_index: usize,lines: &Vec<String>, gear_symbols: &Vec<char>,current_number: &String, gear_map: &mut HashMap<Point, Vec<String>>,
+) {
     let width = lines[i].len();
-    let rows_length = lines.len();
 
-    let (left, right) = clamp_char_bounds(
-        width,
-        (start_index as isize) - 1,
-        end_index + 1
-    );
-    let indices = generate_iterable_indices(i, rows_length);
-    for line_index in indices {
-            let current_line = &lines[line_index];
-            let line_chars = &current_line[left..=right];
-            if has_symbol(line_chars, symbols) {
-                return  true;
-            }
+    let (left, right) = clamp_char_bounds(width, (start_index as isize) - 1, end_index + 1);
+
+    let current_line_chars = &lines[i][left..=right];
+    fill_gears(current_line_chars, i, left, gear_map, current_number, gear_symbols);
+    
+    if i != 0 {
+        let top_chars  = &lines[i - 1][left..=right];
+        fill_gears(top_chars, i - 1, left, gear_map, current_number, gear_symbols);
     }
 
-    false
+    if i != lines.len() - 1 {
+        let bot_chars = &lines[i + 1][left..=right];
+        fill_gears(bot_chars, i + 1, left, gear_map, current_number, gear_symbols);
+    }
 }
 
 fn clamp_char_bounds(width: usize, left: isize, right: usize) -> (usize, usize) {
     (left.clamp(0, width as isize) as usize, right.clamp(0, width - 1))
 }
 
-
-fn generate_iterable_indices(i: usize, rows_length: usize) -> Vec<usize> {
-    let indices = match i {
-        0 => vec![i, i + 1],
-        n if n == rows_length - 1 => vec![i - 1, i],
-        _ => (i - 1..=i + 1).collect(),
-    };
-    
-    indices
-}
-
-fn has_symbol(line: &str, symbols: &Vec<char>) -> bool{
-    for c in line.chars(){
-        if symbols.contains(&c) {
-            return  true;
+fn fill_gears(line: &str, line_index: usize, start_index: usize, gear_map: &mut HashMap<Point, Vec<String>>, num: &String, gear_symbols: &Vec<char>) -> Option<Point> {
+    for (i, char) in line.chars().enumerate() {
+        if gear_symbols.contains(&char) {
+            let column_idx = start_index + i;
+            let gear_point = Point(line_index, column_idx);
+            let gear_nums = gear_map.get_mut(&gear_point);
+            match gear_nums {
+                Some(nums) => {
+                    nums.push(num.clone())
+                },
+                None => {
+                    gear_map.insert(gear_point, vec![num.clone()]);
+                }
+            }
         }
     }
-    false
+    None
 }
