30f5a166b73282c674dcc748fff0ce11976d8dc4
PetarMilisavljevic <milisavljevicpetar6@gmail.com>
2023-12-18 14:35:30 +0100

    day 4 part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,62 +1,92 @@
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader}, cell::RefCell,
+};
 
 #[derive(Debug)]
-struct Card<'a>{
-    title: &'a str,
-    winning_numbers: &'a str,
-    numbers: &'a str
+struct Card {
+    card_number: usize,
+    numbers: Vec<u32>,
+    winning_numbers: Vec<u32>,
+    instances: RefCell<u32>
 }
+
+impl Card{
+    fn new(card_number: usize, numbers: Vec<u32>, winning_numbers: Vec<u32>) -> Self {
+        Self { card_number, numbers, winning_numbers, instances: RefCell::new(1) }
+    }
+
+    fn increase_instances(&self) {
+        *self.instances.borrow_mut() += 1;
+    }
+
+}
+
 fn main() {
     let file = File::open("input.txt").expect("Cannot open file");
     let reader = BufReader::new(file);
-    let cards: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
+    let lines: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
 
-    let mut sum = 0;
+    let cards = parse_lines(lines);
 
-    for card_line in cards{
-        sum = sum + handle_game(&card_line);
+    let mut sum: u32 = 0;
+    for (index, _) in cards.iter().enumerate(){
+        handle_card_recursive(&cards, index);
+    }
+   
+    for card in cards{
+        println!("Card: {} {:?} {:?} {:?}", card.card_number, card.numbers, card.winning_numbers, card.instances);  
+        sum += *card.instances.borrow();
     }
 
-    println!("The total sum of points is: {}", sum);
+    println!("Total instances of cards: {}", sum);
 }
 
-fn handle_game(card_line: &String) -> u32 {
+fn handle_card_recursive(
+    all_cards: &Vec<Card>,
+    parent_card_index: usize,
+) {
+    let parent_card = &all_cards[parent_card_index];
 
-    let mut game_sum: u32 = 0;
+    let mut counter: usize = 0;
 
-    let card = create_card(card_line);
-    
-    let winning_numbers_array: Vec<&str> = card.winning_numbers.split_whitespace().collect();
-    let numbers_array = card.numbers.split_whitespace();
-
-    for number in numbers_array {
-        if winning_numbers_array.contains(&number){
-            if game_sum == 0{
-                game_sum = 1
-            }
-            else {
-                game_sum = game_sum * 2;
-            }
+    for number in &parent_card.numbers {
+        if parent_card.winning_numbers.contains(number) {
+            counter = counter + 1;
         }
     }
 
-    game_sum
+    if counter == 0 {
+        return;
+    }
+    
+    for index in 1..=counter {
+        let child_card_index = parent_card_index + index;
+        let child_card = &all_cards[child_card_index];
+        child_card.increase_instances();
+        handle_card_recursive(all_cards, child_card_index);
+    }
 }
 
-fn create_card(game_line: &String) -> Card {
+fn parse_lines(lines: Vec<String>) -> Vec<Card> {
 
+    let mut cards: Vec<Card> = vec![];
+
+    for line in lines {
+        cards.push(get_card(&line))
+    }
+
+    cards
+}
+
+fn get_card(game_line: &String) -> Card {
     let input: Vec<&str> = game_line.split('|').collect();
-    let title_winning_numbers: Vec<&str> =  input[0].trim().split(':').collect();
-    let title = title_winning_numbers[0].trim();
-    let winning_numbers = title_winning_numbers[1].trim();
-    let numbers = input[1].trim();
 
-    let game = Card {
-        numbers: numbers,
-        title: title,
-        winning_numbers: winning_numbers
-    };
+    let title_winning_numbers: Vec<&str> = input[0].trim().split(':').collect();
+    let winning_numbers = title_winning_numbers[1].trim().split_ascii_whitespace().map(|num| num.parse::<u32>().unwrap()).collect();
+    let card_number = title_winning_numbers[0].trim_start_matches(|c: char| !c.is_digit(10)).parse::<usize>().unwrap();
+    let numbers = input[1].trim().split_whitespace().map(|num| num.parse::<u32>().unwrap()).collect();
 
-    game
-
+    let new_card = Card::new(card_number, numbers, winning_numbers);
+    new_card
 }
