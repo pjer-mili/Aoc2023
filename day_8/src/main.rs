d114a422c36683ec543afd7dbebbbae37f2b16dd
PetarMilisavljevic <milisavljevicpetar6@gmail.com>
2024-02-15 20:30:23 +0100

    day 8 part 2

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -7,15 +7,19 @@ struct Directions {
 }
 
 impl Directions {
+
+    fn reset(&mut self) {
+        self.current_index = 0
+    }
+
     fn get_direction(&self) -> char {
         self.array[self.current_index]
     }
 
     fn traverse(&mut self) {
         if self.current_index < self.array.len() - 1 {
-            self.current_index += 1; 
-        }
-        else {
+            self.current_index += 1;
+        } else {
             self.current_index = 0;
         }
     }
@@ -25,12 +29,12 @@ impl From<String> for Directions {
     fn from(input: String) -> Self {
         let array: Vec<char> = input.chars().collect();
         let current_index: usize = 0;
-        Directions {array, current_index}
+        Directions { array, current_index }
     }
 }
 
 #[derive(Debug)]
-struct Node{
+struct Node {
     value: String,
     left: String,
     right: String,
@@ -42,7 +46,10 @@ impl Node {
     }
 
     fn is_destination(&self) -> bool {
-        self.value == "ZZZ"
+        if self.value.ends_with('Z') {
+            return true;
+        }
+        false
     }
 }
 
@@ -52,22 +59,32 @@ fn main() {
     let (input, nodes) = parse_file(reader);
     let mut directions = Directions::from(input);
 
-    let mut current_node = nodes.iter().next().map(|(_, v)| v).unwrap();
-    let mut steps = 0;
+    let mut start_nodes = get_start_nodes(&nodes);
+    let mut steps_array :Vec<u128> = vec![];
+    
+    for start in start_nodes.iter_mut() {
+        let mut steps_to_z: u128 = 0;
+        let mut current_node = start.clone();
+        directions.reset();
 
-    while !current_node.is_destination() {
-        let direction = directions.get_direction();
-
-        if direction == 'R' {
-            current_node = nodes.get(&current_node.right).unwrap();
-        } else {
-            current_node = nodes.get(&current_node.left).unwrap();
+        while !current_node.is_destination() {
+            let direction = directions.get_direction();
+            match direction {
+                'L' => {
+                    current_node = nodes.get(&current_node.left).unwrap();
+                },
+                'R' => {
+                    current_node = nodes.get(&current_node.right).unwrap();
+                }
+                _ => panic!("Bad input!")
+            }
+            directions.traverse();
+            steps_to_z += 1;
         }
-
-        directions.traverse();
-        steps += 1;        
+        steps_array.push(steps_to_z);
     }
 
+    let steps = lcm_array(steps_array);
     println!("STEPS: {}", steps);
 }
 
@@ -83,10 +100,44 @@ fn parse_file(reader: BufReader<File>) -> (String, BTreeMap<String, Node>) {
 }
 
 fn get_nodes(line: &String) -> Node {
-    let split: Vec<&str> = line.split(&[' ', '=', '(' , ')', ',']).filter(|part| part.ne(&"")).collect();
+    let split: Vec<&str> = line.split(&[' ', '=', '(', ')', ',']).filter(|part| part.ne(&"")).collect();
     Node::new(String::from(split[0]), String::from(split[1]), String::from(split[2]))
 }
 
 fn get_directions(lines: &mut Lines<BufReader<File>>) -> String {
     lines.next().unwrap().expect("Could not get input")
+}
+
+fn get_start_nodes(nodes: &BTreeMap<String, Node>) -> Vec<&Node> {
+    let start_nodes: Vec<&Node> = nodes.values().filter(|&node| node.value.ends_with('A')).collect();
+    start_nodes
+}
+
+fn lcm_array(array: Vec<u128>) -> u128 {
+    array.iter().fold(1, |acc , &x| lcm(acc, x))
+}
+
+fn lcm(first: u128, second: u128) -> u128 {
+    first * second / gcd(first, second)
+}
+
+fn gcd(first: u128, second: u128) -> u128 {
+    let mut max = first;
+    let mut min = second;
+
+    if min > max {
+        let temp = max;
+        max = min;
+        min = temp;
+    }
+
+    loop {
+         let res = max % min;
+         if res == 0 {
+            return min;
+         }
+          
+          max = min;
+          min = res;
+    }
 }
